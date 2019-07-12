5cbf2dfe944830007851222ecef12d3a45bcd0bd
bumingxialuo <690148814@qq.com>
2019-07-12 16:03:17 +0800

    Initial Commit

---
+++ BannerPageControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BannerPageControl
+//
+//  Created by 不明下落 on 2019/7/12.
+//  Copyright © 2019 不明下落. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BannerPageControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BannerPageControl
+//
+//  Created by 不明下落 on 2019/7/12.
+//  Copyright © 2019 不明下落. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
